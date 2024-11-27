c3d2177a7e89cd8a08775ff587132da024fb3d31
vazqdav <vazquezperaza300@gmail.com>
2024-11-26 22:34:31 -0600

    git add

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from pymongo import MongoClient
 from cryptography.fernet import Fernet  # Importamos Fernet
 import hmac
 import hashlib
+import eventlet
+eventlet.monkey_patch()
 
 app = Flask(__name__)
 
@@ -92,10 +94,10 @@ def chat():
     # Recupera mensajes anteriores de la base de datos
     messages = db.messages.find()
     message_list = [{'username': msg['username'], 'text': fernet.decrypt(msg['text']).decode('utf-8'), 'mac': msg['mac']} for msg in messages]
-    
+
     return render_template('chat.html', username=session['username'], messages=message_list, users=user_list)
 
-@app.route('/profile/<username>')
+@app.route('/profile/<username>')  
 def view_profile(username):
     user = db.users.find_one({'username': username})
     if user:
