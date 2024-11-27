de0f309892c8a52cbf60a66f951601d2a46193ce
vazqdav <vazquezperaza300@gmail.com>
2024-11-27 00:41:26 -0600

    git add

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
+import eventlet
+eventlet.monkey_patch()
 from flask import Flask, render_template, redirect, url_for, session, request, flash
 from flask_socketio import SocketIO, send
 from flask_bcrypt import Bcrypt
 from pymongo import MongoClient
 import os
-import eventlet
 
-# Inicializa eventlet para manejar WebSockets correctamente
-eventlet.monkey_patch()
 
 # Configuración de Flask
 app = Flask(__name__)
